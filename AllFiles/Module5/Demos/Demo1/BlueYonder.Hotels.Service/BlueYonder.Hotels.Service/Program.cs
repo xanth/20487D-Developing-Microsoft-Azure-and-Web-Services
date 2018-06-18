e23fb558e41c1a37b50638f42d8f263035889407
shalev zahavi <shalevz@sela.co.il>
2018-06-18 11:35:49 +0300

    Added Starter project for Demo1

---
+++ AllFiles/Module5/Demos/Demo1/BlueYonder.Hotels.Service/BlueYonder.Hotels.Service/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BlueYonder.Hotels.Service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).RunAsService();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
