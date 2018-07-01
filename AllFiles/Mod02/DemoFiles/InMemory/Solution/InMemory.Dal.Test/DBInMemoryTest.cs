2248e8a16b035f3ebdd0ecbf157730589fa7f337
Unknown <roi1godelman@gmail.com>
2018-07-01 15:44:09 +0300

    Mod 2 Demo 5
    Add Instruction 
    Add starter code

--- AllFiles/Mod02/DemoFiles/InMemory/Solution/InMemory.Dal.Test/DBInMemoryTest.cs
+++ AllFiles/Mod02/DemoFiles/InMemory/Solution/InMemory.Dal.Test/DBInMemoryTest.cs
@@ -11,20 +11,49 @@ namespace InMemory.Dal.Test
     [TestClass]
     public class DBInMemoryTest
     {
+        private DbContextOptions<SchoolContext> options = new DbContextOptionsBuilder<SchoolContext>()
+                                                              .UseInMemoryDatabase(databaseName: "TestDatabase")
+                                                              .Options;
+
         [TestMethod]
-        public void GetDataFromDB()
+        public void AddStudentTest()
         {
-            var options = new DbContextOptionsBuilder<SchoolContext>()
-                                .UseInMemoryDatabase(databaseName: "TestDatabase")
-                                .Options;
+            Student student = new Student {Name = "Kari Hensien"};
+            using (var context = new SchoolContext(options))
+            {
+                DbInitializer.Initialize(context);
+                student = context.Students.Add(student).Entity;
+                context.SaveChanges();
+            }
 
             using(var context = new SchoolContext(options))
             {
-                DbInitializer.Initialize(context);
-                List<Student> studentsList = context.Students.ToList();
-                
-                Assert.AreEqual(10,studentsList.Count);
+                var result = context.Students.FirstOrDefault((s)=> s.PersonId == student.PersonId);                
+                Assert.IsNotNull(result);
             }
         }
+
+        [TestMethod]
+        public void UpdateTeacherSalaryTest()
+        {
+            Teacher teacher = new Teacher {Name = "Terry Adams" , Salary = 10000};
+            using (var context = new SchoolContext(options))
+            {
+                DbInitializer.Initialize(context);
+                teacher = context.Teachers.Add(teacher).Entity;
+                context.SaveChanges();
+                teacher.Salary += 10000;
+                context.Teachers.Update(teacher);
+                context.SaveChanges();
+            }
+
+            using(var context = new SchoolContext(options))
+            {
+                var result = context.Teachers.FirstOrDefault((s)=> s.PersonId == teacher.PersonId);
+                
+                Assert.AreEqual(result.Salary,20000);
+            }
+        }
+
     }
 }
---
+++ AllFiles/Mod02/DemoFiles/InMemory/Starter/InMemory.Dal.Test/DBInMemoryTest.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InMemory.Dal.Database;
+using Microsoft.EntityFrameworkCore;
+using InMemory.Dal.Models;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace InMemory.Dal.Test
+{
+    [TestClass]
+    public class DBInMemoryTest
+    {
+
+
+        [TestMethod]
+        public void AddStudentTest()
+        {
+            Student student = new Student {Name = "Kari Hensien"};
+            using (var context = new SchoolContext(options))
+            {
+                DbInitializer.Initialize(context);
+                student = context.Students.Add(student).Entity;
+                context.SaveChanges();
+            }
+
+            using(var context = new SchoolContext(options))
+            {
+                var result = context.Students.FirstOrDefault((s)=> s.PersonId == student.PersonId);                
+                Assert.IsNotNull(result);
+            }
+        }
+   }
+}
