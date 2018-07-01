d5e1605fdd261d8b6f37db7cf0bab50293a57fe6
Unknown <roi1godelman@gmail.com>
2018-07-01 15:52:56 +0300

    Mod 2 Demo 5
    Remove method in Course.cs

--- AllFiles/Mod02/DemoFiles/InMemory/Solution/InMemory.Dal/Models/Course.cs
+++ AllFiles/Mod02/DemoFiles/InMemory/Solution/InMemory.Dal/Models/Course.cs
@@ -11,20 +11,5 @@ namespace InMemory.Dal.Models
         public virtual string Name { get; set; }
         public virtual Teacher CourseTeacher { get; set; }
         public virtual ICollection<Student> Students { get; set; }
-
-        public override string ToString()
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"Course Id: {CourseId}, Name: {Name}");
-            sb.AppendLine($"Teacher name: {CourseTeacher.Name}, Salary: {CourseTeacher.Salary}");
-            sb.AppendLine("Students:");
-
-            foreach (var item in Students)
-	        {
-                sb.AppendLine($"\tStudent name: {item.Name}");
-	        }
-            
-            return sb.ToString();
-        }
     }
 }
--- AllFiles/Mod02/DemoFiles/InMemory/Starter/InMemory.Dal/Models/Course.cs
+++ AllFiles/Mod02/DemoFiles/InMemory/Starter/InMemory.Dal/Models/Course.cs
@@ -12,19 +12,5 @@ namespace InMemory.Dal.Models
         public virtual Teacher CourseTeacher { get; set; }
         public virtual ICollection<Student> Students { get; set; }
 
-        public override string ToString()
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"Course Id: {CourseId}, Name: {Name}");
-            sb.AppendLine($"Teacher name: {CourseTeacher.Name}, Salary: {CourseTeacher.Salary}");
-            sb.AppendLine("Students:");
-
-            foreach (var item in Students)
-	        {
-                sb.AppendLine($"\tStudent name: {item.Name}");
-	        }
-            
-            return sb.ToString();
-        }
     }
 }
