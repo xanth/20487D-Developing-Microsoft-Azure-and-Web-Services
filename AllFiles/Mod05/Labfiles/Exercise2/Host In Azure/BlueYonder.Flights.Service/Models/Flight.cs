4386db86d61ec88110e57e57fcb50bdc2ffb2550
shalevz <shalevz1511@gmail.com>
2018-07-02 11:16:09 +0300

    Update Flight.cs
    Fixed indentation

--- AllFiles/Mod05/Labfiles/Exercise2/Host In Azure/BlueYonder.Flights.Service/Models/Flight.cs
+++ AllFiles/Mod05/Labfiles/Exercise2/Host In Azure/BlueYonder.Flights.Service/Models/Flight.cs
@@ -2,12 +2,12 @@ using System;
 
 namespace BlueYonder.Flights.Service.Models
 {
-		public class Flight
-	{
-		public int Id { get; set; }
-		public string Origin { get; set; }
-		public string Destination { get; set; }
-		public string FlightNumber { get; set; }
-		public DateTime DepartureTime { get; set; }
-	}
+   public class Flight
+   {
+     public int Id { get; set; }
+     public string Origin { get; set; }
+     public string Destination { get; set; }
+     public string FlightNumber { get; set; }
+     public DateTime DepartureTime { get; set; }
+   }
 }
