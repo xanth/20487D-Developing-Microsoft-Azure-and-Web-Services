b41368778aaa4d0c3a17413ae22ad29221554ab5
Unknown <roi1godelman@gmail.com>
2018-08-05 00:42:01 +0300

    mod 4 demo 3

---
+++ AllFiles/Mod04/DemoFiles/AsynchronousActions/Solution/BlueYonder.Flights.Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlueYonder.Flights.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+
+        private string url = "http://xxxxxxxxx/api/image/";
+        public async Task UploadImageAsync(Stream image, string fileName)
+        {
+            HttpContent fileStreamContent = new StreamContent(image);
+            fileStreamContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("form-data") { Name = "file", FileName = fileName };
+            fileStreamContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+            using (var client = new HttpClient())
+            using (var formData = new MultipartFormDataContent())
+            {
+                formData.Add(fileStreamContent);
+                var response = await client.PostAsync(url, formData);
+            }
+        }
+    }
+}
