e048023989c96b8c0664b5c97dc473543c5e1e67
Unknown <roi1godelman@gmail.com>
2018-08-08 14:05:51 +0300

    Mod 4 Demo 3

---
+++ AllFiles/Mod04/DemoFiles/CustomFiltersAndFormatters/Solution/CustomFiltersAndFormatters/Formatter/ImageFormatter.cs
@@ -0,0 +1,27 @@
+﻿using CustomFiltersAndFormatters.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CustomFiltersAndFormatters.Formatter
+{
+    public class ImageFormatter : OutputFormatter 
+    {
+        public ImageFormatter()
+        {
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("image/png"));
+        }
+
+        public override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context)
+        {
+            HttpResponse response = context.HttpContext.Response;
+
+            Value value = context.Object as Value;
+            if(value != null)
+                await response.SendFileAsync((value).Thumbnail);
+
+        }
+
+    }
+}
---
+++ AllFiles/Mod04/DemoFiles/CustomFiltersAndFormatters/Starter/CustomFiltersAndFormatters/Controllers/PassengerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using CustomFiltersAndFormatters.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomFiltersAndFormatters.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PassengerController : ControllerBase
+    {
+        private Value[] _values = new Value[] {
+            new Value{Id =0, Name = "Zero", Thumbnail=@"Images\orderedlist0.png"},
+            new Value{Id =1, Name = "One", Thumbnail=@"Images\orderedlist1.png"},
+            new Value{Id =2, Name = "Two", Thumbnail=@"Images\orderedlist2.png"},
+            new Value{Id =3, Name = "Three", Thumbnail=@"Images\orderedlist3.png"},
+            new Value{Id =4, Name = "Four", Thumbnail=@"Images\orderedlist4.png"},
+            new Value{Id =5, Name = "Five", Thumbnail=@"Images\orderedlist5.png"},
+            new Value{Id =6, Name = "Six", Thumbnail=@"Images\orderedlist6.png"},
+            new Value{Id =7, Name = "Seven", Thumbnail=@"Images\orderedlist7.png"},
+            new Value{Id =8, Name = "Eight", Thumbnail=@"Images\orderedlist8.png"},
+            new Value{Id =9, Name = "Nine", Thumbnail=@"Images\orderedlist9.png"}
+        };
+
+
+        [HttpGet("Photo/{id}")]
+        public Value GetPhoto(int id)
+        {
+            return _values[id];
+        }
+  }
+}
