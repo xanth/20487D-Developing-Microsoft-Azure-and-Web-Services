434f72ed69ebdd8c98f2cc25676d08bf72a9450e
Unknown <roi1godelman@gmail.com>
2018-08-10 19:37:15 +0300

    mod 6 lab 2

---
+++ AllFiles/Mod06/Labfiles/Exercise2/Starter/BlueYonder.Flights.Service/Middleware/TenantMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace BlueYonder.Flights.Service.Middleware
+{
+    public class TenantMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public TenantMiddleware(IConfiguration configuration,RequestDelegate next)
+        {
+            _configuration = configuration;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {          
+            await _next(httpContext);
+        }       
+    }
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class TenantMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseTenantMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TenantMiddleware>();
+        }
+    }
+}
