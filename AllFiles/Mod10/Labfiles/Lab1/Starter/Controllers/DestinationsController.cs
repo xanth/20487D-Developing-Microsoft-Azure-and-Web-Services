f7db14e852561e9019c6ecb2f57db41e116f1eb1
ShellyAharoni <shelly9565@gmail.com>
2018-08-23 14:01:43 +0300

    mod10-lab1-fixing-starter-code
    mod10-lab1-fixing-starter-code

--- AllFiles/Mod10/Labfiles/Lab1/Starter/Controllers/DestinationsController.cs
+++ AllFiles/Mod10/Labfiles/Lab1/Starter/Controllers/DestinationsController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using TestingHttpWithSwagger.Host.Models;
 using Microsoft.AspNetCore.Mvc;
+using  BlueYonder.Flights.Service.Models;
 
 namespace BlueYonder.Flights.Service.Controllers
 {
