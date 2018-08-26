4a8f2bbaa7eb0c6afb48079776bd52287b184610
Avi Avni <avi.avni@gmail.com>
2018-08-26 16:18:30 +0300

    skeleton for mod 10 demos

---
+++ AllFiles/Mod10/DemoFiles/BlueYonder.Hotels.Service/BlueYonder.Hotels.Service/Models/Room.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlueYonder.Hotels.Service.Models
+{
+    [Serializable]
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public int Number { get; set; }
+        public decimal Price { get; set; }
+    }
+}
