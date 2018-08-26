af6000e12cbdcafdb367eea571a42ef1dfdbe82d
ShellyAharoni <shelly9565@gmail.com>
2018-08-26 17:02:41 +0300

    adding-mod10-lab2
    adding-mod10-lab2

---
+++ AllFiles/Mod10/Labfiles/Lab2/Setup/Code/Middleware/MachineNameMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace BlueYonder.Flights.Service.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class MachineNameMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MachineNameMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Response.Headers.Add("X-BlueYonder-Server", Environment.MachineName);
+                
+            await _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MachineNameMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMachineNameMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MachineNameMiddleware>();
+        }
+    }
+}
---
+++ AllFiles/Mod10/Labfiles/Lab2/Setup/createAzureServicesWestUS.ps1
@@ -0,0 +1,51 @@
+﻿﻿### Login to the account
+Login-AzureRmAccount
+
+Write-Host "Please enter your subscription id: " -NoNewline
+$SubscriptionId = Read-Host
+
+Select-AzureRmSubscription -SubscriptionId $SubscriptionId
+
+### get the user name for resources naming in the script
+Write-Host "Please enter your name" -NoNewline
+Write-Host " - no more than 10 characters: " -ForegroundColor Yellow -NoNewline
+$yourName = Read-Host
+
+$yourName = $yourName.ToLower()
+$websiteName = "blueyondermod10lab2t2" + $yourName
+$resourcesGroupName = "Mod10Lab2t2-RG"
+
+
+$location = "WestUS"
+### Create Resource group
+$RG = New-AzureRmResourceGroup -Name "$resourcesGroupName" -Location $location 
+
+### Deploy the webapp
+New-AzureRmResourceGroupDeployment -ResourceGroupName $RG.ResourceGroupName -TemplateFile $PSScriptRoot\template.json -TemplateParameterFile $PSScriptRoot\parameters.json -webappname $websiteName -hostingPlanName "plan$websiteName" -location $location -serverFarmResourceGroup $RG.ResourceGroupName -subscriptionId $SubscriptionId
+
+### Deploy the code to the webapp
+$path = $PSScriptRoot+"\Code"
+Write-Host $path
+cd $path
+$profile = Get-AzureRmWebAppPublishingProfile -ResourceGroupName $RG.ResourceGroupName -Name $websiteName -Format WebDeploy -OutputFile "$path\Properties\PublishProfiles\Azureprofile.xml"
+
+$azureProfilePath = "$path\Properties\PublishProfiles\Azureprofile.xml"
+$publishProfilePath = "$path\Properties\PublishProfiles\Azure.pubxml"
+
+$azureProfileXml = [xml](Get-Content $azureProfilePath)
+$publishProfileXml = [xml](Get-Content $publishProfilePath)
+
+$publishProfileXml.Project.PropertyGroup.PublishSiteName = $websiteName
+$MSDeployPublishProfile = $azureProfileXml.publishData.publishProfile | where {$_.publishMethod -eq 'MSDeploy'}
+$publishProfileXml.Project.PropertyGroup.UserName = $MSDeployPublishProfile.userName
+$publishProfileXml.Project.PropertyGroup.Password = $MSDeployPublishProfile.userPWD
+
+$publishProfileXml.Save($publishProfilePath)
+
+&dotnet publish /p:PublishProfile=Azure /p:Configuration=Release
+
+### Get the webapp site url
+$webapp = Get-AzureRmWebApp -ResourceGroupName $RG.ResourceGroupName -Name $websiteName 
+$urlname = $webapp.DefaultHostName
+$siteurl = "https://$urlname"
+Write-Host "The WebApp URL is: $siteurl"
