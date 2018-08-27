d0f5755e97c59ca0d17733a57b4ac64cb75bb60f
Unknown <roi1godelman@gmail.com>
2018-08-27 11:48:22 +0300

    Mod 8 Lab 1
    Add ex 1 code

---
+++ AllFiles/Mod08/Labfiles/Lab1/Blueyonder.Service/Controllers/FlightsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blueyonder.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+       
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            throw new Exception($"Blueyonder don't have a flight with this id = {id}");
+        }
+     
+    }
+}
