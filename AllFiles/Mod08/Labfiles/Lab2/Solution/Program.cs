e3dfcbc3d0ffa532da0ea6bf82a97ef05b24ac0e
Unknown <roi1godelman@gmail.com>
2018-08-30 08:21:08 +0300

    Mod 8 Lab 2
    updated solution

--- AllFiles/Mod08/Labfiles/Lab2/Solution/Program.cs
+++ AllFiles/Mod08/Labfiles/Lab2/Solution/Program.cs
@@ -19,6 +19,7 @@ namespace BlueYonder.Flights.Service
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseApplicationInsights();
     }
 }
