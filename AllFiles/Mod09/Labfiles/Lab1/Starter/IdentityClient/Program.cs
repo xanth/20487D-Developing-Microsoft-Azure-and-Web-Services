c94428e86d01e13a1d59676a1311ba8a857e3ca3
Unknown <avichaigez@gmail.com>
2018-12-06 14:33:48 +0200

    mod09 lab01
    new instruction added
    starter files code added

---
+++ AllFiles/Mod09/Labfiles/Lab1/Starter/IdentityClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            var postRes = await client.PostAsync("https://localhost:5001/api/user/register", new StringContent("{ \"Username\": \"avigi@dima.com\", \"Password\": \"AsdAsd123123!!!\" }", Encoding.UTF8, "application/json"));
+            var token = await postRes.Content.ReadAsStringAsync();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var res = await client.GetAsync("https://localhost:5001/api/values");
+            var str = await res.Content.ReadAsStringAsync();
+            Console.WriteLine(str);
+        }
+    }
+}
