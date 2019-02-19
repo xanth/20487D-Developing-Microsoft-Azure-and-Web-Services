078f888dd313507cd665eb2889fb01a2d57fcd8c
vikriu <vormex@gmail.com>
2019-02-19 14:10:40 +0200

    Update CacheAttribute.cs
    Typo

--- AllFiles/Mod04/LabFiles/Lab1/Solution/BlueYonder.Hotels/BlueYonderHotels.Service/Attirbutes/CacheAttribute.cs
+++ AllFiles/Mod04/LabFiles/Lab1/Solution/BlueYonder.Hotels/BlueYonderHotels.Service/Attirbutes/CacheAttribute.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BlueYonderHotels.Service.Attirbutes
+namespace BlueYonderHotels.Service.Attributes
 {
     public class CacheAttribute : ActionFilterAttribute
     {
