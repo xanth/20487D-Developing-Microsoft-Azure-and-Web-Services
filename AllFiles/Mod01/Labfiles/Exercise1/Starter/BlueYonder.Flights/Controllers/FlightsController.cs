f4ece829619e751c7f78cef11b12ba3ead715d2f
Rhys Williams <rhys.williams@rightcrowd.com>
2019-05-22 11:48:33 +1000

    Module 1 Exercise 3

--- AllFiles/Mod01/Labfiles/Exercise1/Starter/BlueYonder.Flights/Controllers/FlightsController.cs
+++ AllFiles/Mod01/Labfiles/Exercise1/Starter/BlueYonder.Flights/Controllers/FlightsController.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlueYonder.Flights.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlueYonder.Flights.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class FlightsController : ControllerBase
     {
+        private readonly FlightsContext context;
+
+        public FlightsController(FlightsContext context)
+        {
+            this.context = context;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        public ActionResult<IEnumerable<Flight>> Get() => context.Flights.ToList();
 
         // GET api/values/5
         [HttpGet("{id}")]
@@ -26,8 +31,12 @@ namespace BlueYonder.Flights.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> PostAsync([FromBody] Flight flight)
         {
+            context.Flights.Add(flight);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), flight.Id);
         }
 
         // PUT api/values/5
